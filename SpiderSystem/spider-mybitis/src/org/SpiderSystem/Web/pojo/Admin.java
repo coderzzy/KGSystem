3f231bfd9d59c68132b88447ee5ad4b34a828bf4
coder_zzy <zyzhang_dev@163.com>
2018-05-03 14:03:00 +0800

    reset to 2018.04.25

---
+++ SpiderSystem/spider-mybitis/src/org/SpiderSystem/Web/pojo/Admin.java
@@ -0,0 +1,83 @@
+package org.SpiderSystem.Web.pojo;
+
+public class Admin {
+    private Integer id;
+
+    private String email;
+
+    private String password;
+
+    private String name;
+
+    private String telephone;
+
+    private String qq;
+
+    private String twitter;
+
+    private String intro;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email == null ? null : email.trim();
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password == null ? null : password.trim();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone == null ? null : telephone.trim();
+    }
+
+    public String getQq() {
+        return qq;
+    }
+
+    public void setQq(String qq) {
+        this.qq = qq == null ? null : qq.trim();
+    }
+
+    public String getTwitter() {
+        return twitter;
+    }
+
+    public void setTwitter(String twitter) {
+        this.twitter = twitter == null ? null : twitter.trim();
+    }
+
+    public String getIntro() {
+        return intro;
+    }
+
+    public void setIntro(String intro) {
+        this.intro = intro == null ? null : intro.trim();
+    }
+}
